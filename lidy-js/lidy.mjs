48051fb04d96020f3d34ecdee6f0bc02ef85ac40
Xavier Talon <reivaxt@gmail.com>
2021-05-03 22:58:01 +0200

    yaml parsing of the source code and of the grammar

--- lidy-js/lidy.mjs
+++ lidy-js/lidy.mjs
@@ -1,5 +1,6 @@
 import fs   from 'fs'
 import path from 'path'
+import { PerformanceObserver } from 'perf_hooks'
 
 import {
     Document,
@@ -11,14 +12,55 @@ import {
     LineCounter
   } from 'yaml'
 
-  const lineCounter = new LineCounter()
-  const doc = parseDocument(`
+function parse_dsl(ctx, dsl_data, top_rule) {
+  let dsl_doc = parseDocument(dsl_data)
+  if ( ! dsl_doc ) throw Error("ERROR : can not parse dsl ")
+  if (dsl_doc.errors.length > 0 || dsl_doc.warnings.lentgh > 0) throw Error("ERROR : errors parsing dsl")
+  if (!isMap(dsl_doc.contents)) throw Error("ERROR : no grammar rules found")
+  ctx.dsl_rules = dsl_doc.contents
+  if (! ctx.dsl_rules.has(top_rule)) throw Error("ERROR : no rule labeled '" + top_rule + "' in the grammar")
+  return ctx
+}
 
+function parse_src(ctx, src_data) {
+  ctx.lineCounter = new LineCounter()
+  let src_doc = parseDocument(src_data, {lineCounter: ctx.lineCounter})
+  if ( ! src_doc ) throw Error("ERROR : can not parse the provided source code.")
+  ctx.src = src_doc.contents
+  ctx.errors   = src_doc.errors
+  ctx.warnings = src_doc.warnings
+  ctx.yaml_ok  = (ctx.errors.length == 0) && (ctx.warnings.length == 0)
+  return ctx
+}
 
-  toto: 
-    tutu: 7 #comment
-    titi: 5`, {lineCounter:lineCounter})
-  let value = doc.contents
-  console.log(value)
-  console.log(lineCounter.linePos(21))
-  console.log(lineCounter.lineStarts)
+// main lidy function to parse source code using lidy grammar
+function parse(input) { 
+  // input is an object with two attributes :
+  //  - one to provide the source code to code, 
+  //    among 'src_file' and 'src_data' depending on whether you want to indicate a file or a text.
+  //  - one to provide the lidy grammar to use,  
+  //    among 'dsl_file' and 'dsl_data' depending on whether you want to indicate a file or a text.
+  // if a both filename and data are provided, content of the file is used rather than data
+  if (input.dsl_file != null) {
+    input.dsl_data = fs.readFileSync(input.dsl_file, 'utf8')
+  } else { input.dsl_file = 'stdin' }
+  if (input.dsl_data == null) { throw Error("No dsl definition found from provided input") }
+
+  if (input.src_file != null) {
+    input.src_data = fs.readFileSync(input.src_file, 'utf8')
+  } else { input.src_file = 'stdin' }
+  if (input.dsl_data == null) { throw Error("No source code found from provided input") }
+
+  if (!input.keyword) input.keyword = 'main' // use 'top' rule of the grammat as entry point if none is provided
+
+  let ctx = {} // initialise context
+
+  ctx = parse_dsl(ctx, input.dsl_data, input.keyword) // yaml parsing of the grammar rules
+  ctx = parse_src(ctx, input.src_data)                // yaml parsing of the source code 
+//  ctx = parse_src_dsl(ctx, input.keyword)       // dsl parsing of the source code
+
+  return ctx
+}
+
+let ctx = parse({src_data: "", dsl_file: "./schema.lidy.yaml"})
+console.log(ctx)
